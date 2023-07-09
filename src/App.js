b5b7395521f0c57a0f6415d6648e26c6a32140f8
mukaji-k <just.margaux.b@gmail.com>
2023-07-09 17:45:37 +0200

    new update

--- src/App.js
+++ src/App.js
@@ -6,29 +6,32 @@ function App() {
   const [query, setQuery] = useState("");
 
   const search = (data)=>{
-    return Fruits.filter(fruit=>fruit.name.toLocaleLowerCase().toLocaleUpperCase().includes(query));
+    return data.filter(fruit=>fruit.name.includes(query));
   }
-
+console.log(query);
   return (
     <div className="App">
       <div className='title-div'>
         <h1 className='title'>iFruits - search fruits images</h1>
       </div>
-      <input placeholder='write your fruit name here...' type='text' className='search' onChange={(e)=> setQuery(e.target.value)} />
+      <input placeholder="write your fruit's name here..." 
+      type='text' className='search' 
+      onChange={(e)=> setQuery(e.target.value)} />
 
-      <ul>
-      {Fruits.filter(fruit=>fruit.name.includes(query)).map((fruit)=>{
-        return(
-          <>
-            <li className='listItem' key={fruit.id}>
-              <img src={fruit.img} alt={fruit.name} width='300px' data={search(Fruits)} />
-            </li>
-          </>
-          )
-        })}
-      </ul>
+   <ul>
+    {Fruits.filter(fruit=>fruit.name.includes(query)).map((fruit)=>{
+      return(
+        <>
+          <li className='listItem' key={fruit.id}>
+            <img src={fruit.img} alt={fruit.name} width='300px' data={search(Fruits)} />
+          </li>
+        </>
+        )
+      })}
+    </ul>
 
     </div>
+   
   );
 }
 
--- src/Fruits.js
+++ src/Fruits.js
@@ -1,192 +1,192 @@
 const Fruits = [
     {
         "id": 1,
-        "name": "apple",
+        "name": "apple, APPLE",
         "img": "https://upload.wikimedia.org/wikipedia/commons/3/35/Golden_delicious_apple.jpg"
     },
     {
         "id": 2,
-        "name": "lemon",
+        "name": "lemon, LEMON",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Lemon_-_whole_and_split.jpg/1200px-Lemon_-_whole_and_split.jpg"
     },
     {
         "id": 3,
-        "name": "orange",
+        "name": "orange, ORANGE",
         "img": "https://upload.wikimedia.org/wikipedia/commons/b/b0/OrangeBloss_wb.jpg"
     },
     {
         "id": 4,
-        "name": "pinnapple",
+        "name": "pinnapple, PINNAPPLE",
         "img": "https://upload.wikimedia.org/wikipedia/commons/7/74/%E0%B4%95%E0%B5%88%E0%B4%A4%E0%B4%9A%E0%B5%8D%E0%B4%9A%E0%B4%95%E0%B5%8D%E0%B4%95.jpg"
     },
     {
         "id": 5,
-        "name": "mango",
+        "name": "mango, MANGO",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/MANGO249.JPG/1200px-MANGO249.JPG"
     },
     {
         "id": 6,
-        "name": "kiwi",
+        "name": "kiwi, KIWI",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Kiwi_%28Actinidia_chinensis%29_1_Luc_Viatour.jpg/1200px-Kiwi_%28Actinidia_chinensis%29_1_Luc_Viatour.jpg"
     },
     {
         "id": 7,
-        "name": "strawberry",
+        "name": "strawberry, STRAWBERRY",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Garden_strawberry_%28Fragaria_%C3%97_ananassa%29_single.jpg/1200px-Garden_strawberry_%28Fragaria_%C3%97_ananassa%29_single.jpg"
     },
     {
         "id": 8,
-        "name": "raspberry",
+        "name": "raspberry, RASPBERRY",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Raspberries05.jpg/1200px-Raspberries05.jpg"
     },
     {
         "id": 9,
-        "name": "blueberry",
+        "name": "blueberry, BLUEBERRY",
         "img": "https://upload.wikimedia.org/wikipedia/commons/8/88/PattsBlueberries.jpg"
     },
     {
         "id": 10,
-        "name": "grapefruit",
+        "name": "grapefruit, GRAPEFRUIT",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Grapefruits_-_whole-halved-segments.jpg/1200px-Grapefruits_-_whole-halved-segments.jpg"
     },
     {
         "id": 11,
-        "name": "grapes",
+        "name": "grapes, GRAPES",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Close_up_grapes.jpg/1200px-Close_up_grapes.jpg"
     },
     {
         "id": 12,
-        "name": "cherry",
+        "name": "cherry, CHERRY",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Cherries_summerland.jpg/1200px-Cherries_summerland.jpg"
     },
     {
         "id": 13,
-        "name": "banana",
+        "name": "banana, BANANA",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Green_yellow_bananas_dsc07775.jpg/1200px-Green_yellow_bananas_dsc07775.jpg"
     },
     {
         "id": 14,
-        "name": "melon",
+        "name": "melon, MELON",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Cantaloupe_and_canary_melon.jpg/1200px-Cantaloupe_and_canary_melon.jpg"
     },
     {
         "id": 15,
-        "name": "watermelon",
+        "name": "watermelon, WATERMELON",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Watermelon_cross_BNC.jpg/1200px-Watermelon_cross_BNC.jpg"
     },
     {
         "id": 16,
-        "name": "pear",
+        "name": "pear, PEAR",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Pear_DS.jpg/1200px-Pear_DS.jpg"
     },
     {
         "id": 17,
-        "name": "plum",
+        "name": "plum, PLUM",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Plums_African_Rose_-_whole%2C_halved_and_slice.jpg/1200px-Plums_African_Rose_-_whole%2C_halved_and_slice.jpg"
     },
     {
         "id": 18,
-        "name": "coconut",
+        "name": "coconut, COCONUT",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/2019_Jan_15_-_Kumbh_Mela_-_Coconuts_For_Sale.jpg/1200px-2019_Jan_15_-_Kumbh_Mela_-_Coconuts_For_Sale.jpg"
     },
     {
         "id": 19,
-        "name": "pomegranate",
+        "name": "pomegranate, POMEGRANATE",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Pomegranate_fruit_-_whole_and_piece_with_arils.jpg/1200px-Pomegranate_fruit_-_whole_and_piece_with_arils.jpg"
     },
     {
         "id": 20,
-        "name": "passionfruit",
+        "name": "passionfruit, PASSIONFRUIT",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Passion_fruits_-_whole_and_halved.jpg/1200px-Passion_fruits_-_whole_and_halved.jpg"
     },
     {
         "id": 21,
-        "name": "avocado",
+        "name": "avocado, AVOCADO",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Avocado_Hass_-_single_and_halved.jpg/1200px-Avocado_Hass_-_single_and_halved.jpg"
     },
     {
         "id": 22,
-        "name": "mandarin",
+        "name": "mandarin, MANDARIN",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Mandarins_-_whole_and_halved.jpg/1200px-Mandarins_-_whole_and_halved.jpg"
     },
     {
         "id": 23,
-        "name": "apricot",
+        "name": "apricot, APRICOT",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Apricot_and_cross_section.jpg/1200px-Apricot_and_cross_section.jpg"
     },
     {
         "id": 24,
-        "name": "fig",
+        "name": "fig, FIG",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Fig_interior.jpg/1200px-Fig_interior.jpg"
     },
     {
         "id": 25,
-        "name": "lychee",
+        "name": "lychee, LYCHEE",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Litchi_chinensis_fruits.JPG/1200px-Litchi_chinensis_fruits.JPG"
     },
     {
         "id": 26,
-        "name": "papaya",
+        "name": "papaya, PAPAYA",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Papaya_-_longitudinal_section.jpg/1200px-Papaya_-_longitudinal_section.jpg"
     },
     {
         "id": 27,
-        "name": "guava",
+        "name": "guava, GUAVA",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Guava_ID.jpg/1200px-Guava_ID.jpg"
     },
     {
         "id": 28,
-        "name": "carambola",
+        "name": "carambola, CARAMBOLA",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Carambolas765pt.jpg/1200px-Carambolas765pt.jpg"
     },
     {
         "id": 29,
-        "name": "pitaya",
+        "name": "pitaya, PITAYA",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Dragonfruit_Chiayi_market.jpg/1200px-Dragonfruit_Chiayi_market.jpg"
     },
     {
         "id": 30,
-        "name": "chikoo",
+        "name": "chikoo, CHIKOO",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/%E0%B4%B8%E0%B4%AA%E0%B5%8D%E0%B4%AA%E0%B5%8B%E0%B4%9F%E0%B5%8D%E0%B4%9F.jpg/1200px-%E0%B4%B8%E0%B4%AA%E0%B5%8D%E0%B4%AA%E0%B5%8B%E0%B4%9F%E0%B5%8D%E0%B4%9F.jpg"
     },
     {
         "id": 31,
-        "name": "jackfruit",
+        "name": "jackfruit, JACKFRUIT",
         "img": "https://upload.wikimedia.org/wikipedia/commons/f/f3/Jack_Fruit_Opened.jpg"
     },
     {
         "id": 32,
-        "name": "longan",
+        "name": "longan, LONGAN",
         "img": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Frutos_Ex%C3%B3ticos-LonganFruit-002.JPG"
     },
     {
         "id": 33,
-        "name": "kaki",
+        "name": "kaki, KAKI",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Persimmon_%28fuyu%29.jpg/1200px-Persimmon_%28fuyu%29.jpg"
     },
     {
         "id": 34,
-        "name": "rhubarb",
+        "name": "rhubarb, RHUBARB",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Rheum_rhabarbarum.2006-04-27.uellue.jpg/1200px-Rheum_rhabarbarum.2006-04-27.uellue.jpg"
     },
     {
         "id": 35,
-        "name": "blackcurrant",
+        "name": "blackcurrant, BLACKCURRANT",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ribes_nigrum_a1.JPG/1200px-Ribes_nigrum_a1.JPG"
     },
     {
         "id": 36,
-        "name": "lime",
+        "name": "lime, LIME",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Citrus_hystrix_dsc07772.jpg/1200px-Citrus_hystrix_dsc07772.jpg"
     },
     {
         "id": 37,
-        "name": "olive",
+        "name": "olive, OLIVE",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Olive_fruit_on_the_branch_%282007%29.jpg/1200px-Olive_fruit_on_the_branch_%282007%29.jpg"
     },
     {
         "id": 38,
-        "name": "mulberry",
+        "name": "mulberry, MULBERRY",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Wild_mulberry.jpg/1200px-Wild_mulberry.jpg"
     }
 ]
